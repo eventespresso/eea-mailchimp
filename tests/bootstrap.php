16bebf6e0f297548a3b63ca298644295de93912e
Darren Ethier <darren@roughsmootheng.in>
2017-04-20 15:09:12 -0400

    update bootstrap and config for tests on eea-mailchimp [touch: 10623]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,26 +1,21 @@
 <?php
 /**
- * Bootstrap for EE4 MailChimp Unit Tests
- *
- * @since       2.0.0
- * @package     EE4 MailCHimp
- * @subpackage  Tests
+ * Bootstrap for eea-mailchimp tests
  */
 
-require( dirname( __FILE__ ) . '/includes/define-constants.php' );
-if ( ! file_exists( WP_TESTS_DIR . '/includes/functions.php' ) ) {
-    die( "The WordPress PHPUnit test suite could not be found.\n" );
-}
+use EETests\bootstrap\AddonLoader;
 
-require_once WP_TESTS_DIR . '/includes/functions.php';
+$core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+require $core_tests_dir . 'includes/CoreLoader.php';
+require $core_tests_dir . 'includes/AddonLoader.php';
 
-function _install_and_load_core_and_ee4_mc() {
-    require EE_TESTS_DIR . 'includes/loader.php';
-    require EE4_MC_TESTS_DIR . 'includes/loader.php';
-}
-tests_add_filter( 'muplugins_loaded', '_install_and_load_core_and_ee4_mc' );
+define('EE4_MC_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
+define('EE4_MC_TESTS_DIR', EE4_MC_PLUGIN_DIR . 'tests/');
 
-require WP_TESTS_DIR . '/includes/bootstrap.php';
 
-//Load the EE_specific testing tools
-require EE_TESTS_DIR . 'includes/EE_UnitTestCase.class.php';
+$addon_loader = new AddonLoader(
+    EE4_MC_TESTS_DIR,
+    EE4_MC_PLUGIN_DIR,
+    'ee4-mailchimp.php'
+);
+$addon_loader->init();
