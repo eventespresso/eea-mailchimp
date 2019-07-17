c961b365f6878018c981aeca5e5e677e7b4af547
Josh Feck <josh@nxnweb.com>
2019-07-17 12:29:54 -0400

    remove legacy code

--- admin/mailchimp/Mailchimp_Admin_Page_Init.core.php
+++ admin/mailchimp/Mailchimp_Admin_Page_Init.core.php
@@ -4,44 +4,6 @@
 *
 **/
 
-// Different methods for EE4.3 and EE4.4.
-if ('4.4.0' > EVENT_ESPRESSO_VERSION) {
-
-    class Mailchimp_Admin_Page_Init extends EE_Admin_Page_Init
-    {
-
-        public function __construct()
-        {
-            do_action('AHEE_log', __FILE__, __FUNCTION__, '');
-            define('EE_MAILCHIMP_LABEL', __('Mailchimp', 'event_espresso'));
-            define('EE_MAILCHIMP_ADMIN_URL', admin_url('admin.php?page=' . ESPRESSO_MAILCHIMP_SETTINGS_PAGE_SLUG));
-            define('EE_MAILCHIMP_TEMPLATE_PATH', ESPRESSO_MAILCHIMP_ADMIN_DIR . 'mailchimp/templates/');
-            parent::__construct();
-            $this->_folder_path = ESPRESSO_MAILCHIMP_ADMIN_DIR . 'mailchimp' . DS;
-        }
-
-        protected function _set_init_properties()
-        {
-            $this->label = EE_MAILCHIMP_LABEL;
-            $this->menu_label = EE_MAILCHIMP_LABEL;
-            $this->menu_slug = ESPRESSO_MAILCHIMP_SETTINGS_PAGE_SLUG;
-            $this->capability = 'manage_options';
-        }
-
-        public function get_menu_map()
-        {
-            $map = array(
-            'group' => 'settings',
-            'menu_order' => 40,
-            'show_on_menu' => true,
-            'parent_slug' => 'espresso_events'
-            );
-            return $map;
-        }
-    }
-
-} else {
-
     class Mailchimp_Admin_Page_Init extends EE_Admin_Page_Init
     {
 
@@ -74,5 +36,3 @@ if ('4.4.0' > EVENT_ESPRESSO_VERSION) {
             ));
         }
     }
-
-}
